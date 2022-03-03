8ed5f1fc926b9cf75e70ac92da948ba67ea26c52
shafi10 <shafiswe@gmail.com>
2022-03-03 21:51:04 +0600

    changes on client

--- routes/loan.js
+++ routes/loan.js
@@ -1,14 +1,19 @@
-const express = require('express')
-const router = express.Router()
-const {check} = require('express-validator/check')
-const { postLoan, getLoan, getSingleLoan, updateLoan, deleteLoan} = require('../controllers/loanCtrl')
-const auth = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const { check } = require("express-validator/check");
+const {
+  postLoan,
+  getLoan,
+  getSingleLoan,
+  updateLoan,
+  deleteLoan,
+} = require("../controllers/loanCtrl");
+const auth = require("../middleware/auth");
 
-router.post('/',auth, postLoan);
-router.get('/',auth, getLoan);
-router.get('/single/:id',auth, getSingleLoan)
-router.put('/update/:id',auth, updateLoan);
-router.post('/delete/:id',auth, deleteLoan);
+router.post("/", auth, postLoan);
+router.get("/", auth, getLoan);
+router.get("/single/:id", auth, getSingleLoan);
+router.put("/update/:id", auth, updateLoan);
+router.post("/delete/:id", auth, deleteLoan);
 
-
-module.exports = router
+module.exports = router;
